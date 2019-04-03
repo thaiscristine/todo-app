7d6706a43425b73930212e7e2e52e62291b7ab5c
Thais Cristine <this.cristine06@gmail.com>
2019-04-03 10:08:33 +0300

    todo and about components

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Todo from '../todo/todo.jsx'
+import About from '../about/about.jsx'
 
 export default props => (
     <div className='container'>
-        <h1>Teste</h1>
+        <Todo />
+        <About />
     </div>
 )
