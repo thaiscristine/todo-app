c7c317624a7a11dca816a21929bdfdf4ef5f8680
Thais Cristine <this.cristine06@gmail.com>
2019-04-03 13:01:57 +0300

    setting routes

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,14 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Todo from '../todo/todo.jsx'
-import About from '../about/about.jsx'
+import Routes from './routes.jsx'
 import Menu from '../template/menu.jsx'
 
 export default props => (
     <div className='container'>
         <Menu />
-        <Todo />
-        <About />
+        <Routes />
     </div>
 )
