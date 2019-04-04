db9200df6bfdcfe7f830daf5e0057a6728514d0f
Thais Cristine <this.cristine06@gmail.com>
2019-04-04 10:00:48 +0300

    form structure

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default props => (
     <div>
-        <h1>Form</h1>
+        <div className="todoForm" role="form">
+            <div className="col-xs-12 col-sm-9 col-md-10">
+                <input type="text" className="form-control" id="description" placeholder="Adicione uma tarefa"></input>
+            </div>
+            <div className="col-xs-12 col-sm-3 col-md-2">
+                <button className="btn btn-primary">
+                    <i className="fa fa-plus"></i>
+                </button>
+            </div>
+        </div>
     </div>
 )
