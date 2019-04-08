7adeff4a0ae8412553be18af24f2c0907adcb7d4
Thais Cristine <this.cristine06@gmail.com>
2019-04-08 17:08:21 +0300

    handleSearch and css

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,17 +2,32 @@ import React from 'react'
 import Grid from '../template/grid.jsx'
 import IconButton from '../template/iconButton.jsx'
 
-export default props => (
-    <div>
-        <div className="todoForm" role="form">
+export default props => {
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+
+    return (
+        <div role='form' className='todoForm'>
             <Grid cols='12 9 10'>
-                <input type="text" className="form-control" id="description" placeholder="Adicione uma tarefa"
-                value={props.description} onChange={props.handleChange}></input>
+                <input id='description' className='form-control'
+                    placeholder='Adicione uma tarefa'
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}
+                    value={props.description}></input>
             </Grid>
             <Grid cols='12 3 2'>
                 <IconButton style='primary' icon='plus'
-                onClick={props.handleAdd}></IconButton>
+                    onClick={props.handleAdd}></IconButton>
+                <IconButton style='info' icon='search'
+                    onClick={props.handleSearch}></IconButton>
+                <IconButton style='default' icon='close'
+                    onClick={props.handleClear}></IconButton>
             </Grid>
         </div>
-    </div>
-)
+    )
+}
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,30 +5,30 @@ module.exports = {
     entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename: './app.js',
+        filename: './app.js'
     },
     devServer: {
         port: 8080,
-        contentBase: './public'
+        contentBase: './public',
     },
     resolve: {
-        extentions: ['', 'js', 'jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
     },
-    plugins: [
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
-        test: /.js[x]?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-            presets: ['es2015', 'react'],
-            plugins: ['transform-object-rest-spread']
-        }
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
         }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
