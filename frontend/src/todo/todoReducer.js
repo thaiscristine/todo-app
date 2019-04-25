ebcbf382955fb1686c17b61be9ee7910b06a5e65
Thais Cristine <this.cristine06@gmail.com>
2019-04-25 09:49:11 +0300

    action creator search

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -21,6 +21,8 @@ export default (state = INITIAL_STATE, action) => {
             return {
                 ...state, description: action.payload
             }
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data }
         default:
             return state
     }
