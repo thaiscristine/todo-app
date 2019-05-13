0d236a343845e7665d885386d0042f082bd2cfd8
Thais Cristine <this.cristine06@gmail.com>
2019-05-13 16:41:23 +0300

    improving searchAction

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,13 +1,13 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-
-import Grid from '../template/grid.jsx'
-import IconButton from '../template/iconButton.jsx'
-import { add, changeDescription, search, clear } from './todoActions.js'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+import { add, changeDescription, search, clear } from './todoActions'
 
 class TodoForm extends Component {
+
     constructor(props) {
         super(props)
         this.keyHandler = this.keyHandler.bind(this)
@@ -18,33 +18,32 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
-        const { add, clear, search, description } = this.props
-
-        if (e.key === 'Enter') {
+        const { add, search, description, clear } = this.props
+        
+        if(e.key === 'Enter'){
             e.shiftKey ? search() : add(description)
-        } else if (e.key === 'Escape') {
+        } else if(e.key === 'Escape') {
             clear()
         }
     }
-    render(){
-
+  render() {
         const { add, search, description } = this.props
         return (
             <div role='form' className='todoForm'>
                 <Grid cols='12 9 10'>
                     <input id='description' className='form-control'
-                        placeholder='Adicione uma tarefa'
-                        onChange={this.props.changeDescription}
-                        onKeyUp={this.keyHandler}
-                        value={this.props.description}></input>
+                    placeholder='Adicione uma tarefa'
+                    onChange={this.props.changeDescription}
+                    onKeyUp={this.keyHandler}
+                    value={this.props.description}></input>
                 </Grid>
                 <Grid cols='12 3 2'>
                     <IconButton style='primary' icon='plus'
-                        onClick={() => add(description)}></IconButton>
+                    onClick={() => add(description)}></IconButton>
                     <IconButton style='info' icon='search'
-                        onClick={() => search}></IconButton>
+                    onClick={search}></IconButton>
                     <IconButton style='default' icon='close'
-                        onClick={this.props.clear}></IconButton>
+                    onClick={this.props.clear}></IconButton>
                 </Grid>
             </div>
         )
@@ -52,6 +51,6 @@ class TodoForm extends Component {
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
-const mapDispatchToProps = dispatch => 
-    bindActionCreators({ add, changeDescription, search, clear }, dispatch)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ add, changeDescription, search, clear }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,31 +1,14 @@
-const INITIAL_STATE ={
-    description: 'Ler livro',
-    list: [{
-        _id:1,
-        description: 'paga asdasdr fatura',
-        done: true
-    },{
-        _id:2,
-        description: 'test',
-        done:false
-    },{
-        _id: 3,
-        description: 'reuniao',
-        done: false
-    }]
-}
+const INITIAL_STATE = { description: '', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
-    switch(action.type) {
-        case 'DESCRIPTION_CHANGED':
-            return {
-                ...state, description: action.payload
-            }
-        case 'TODO_SEARCHED':
-            return { ...state, list: action.payload.data }
-        case 'TODO_CLEAR':
-            return {...state, description: ''}
-        default:
-            return state
-    }
+  switch(action.type) {
+    case 'DESCRIPTION_CHANGED':
+        return { ...state, description: action.payload }
+    case 'TODO_SEARCHED':
+        return { ...state, list: action.payload }
+    case 'TODO_CLEAR':
+        return { ...state, description: ''}
+    default:
+        return state
+  }
 }
